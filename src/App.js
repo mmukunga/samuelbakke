f93f2bd91aa0235e6118637bcf008aa773f73bda
Simon Mukunga <mmukunga@gmail.com>
2022-03-16 16:49:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -63,17 +63,6 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <h1>A movie database</h1>
         <h4>By Abstract </h4>
       </header>
