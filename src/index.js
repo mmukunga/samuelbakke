822cd4d25067c7e1061f43c2b1c80d05d8f054bb
Simon Mukunga <mmukunga@gmail.com>
2022-03-16 18:34:21 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(
   <React.StrictMode>
